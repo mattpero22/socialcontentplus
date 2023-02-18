588e676270a737b43a9074919c341f98fe5b2ce5
matt pero <mattpero22@gmail.com>
2023-02-18 10:41:27 -0500

    added logo image to Nav bar and structured HTML

--- main/src/components/Nav.jsx
+++ main/src/components/Nav.jsx
@@ -1,6 +1,23 @@
+import { HashLink } from 'react-router-hash-link'
+
 const Nav = () => {
     return <>
-        <div id="nav">Nav Component</div>
+        <div id="nav">
+            <div id="logo">
+                <img id="navLogo" alt="Social Content +"src={`./img/logo1.jpg`}></img>
+            </div>
+            <ul id="navButtons">
+                <li id="homeButton" className="navButton">
+                    Home
+                </li>
+                <li id="aboutButton" className="navButton">
+                    Contact
+                </li>
+                <li id="contactButton" className="navButton">
+                    About
+                </li>
+            </ul>
+        </div>
     </>
 }
 
