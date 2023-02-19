b0b2a8814e66e26b13617cb2a7afe16bd82cef77
matt pero <mattpero22@gmail.com>
2023-02-19 08:48:57 -0500

    update on logo, styled nav component with basic layout using flexbox

--- main/src/components/Nav.jsx
+++ main/src/components/Nav.jsx
@@ -4,17 +4,17 @@ const Nav = () => {
     return <>
         <div id="nav">
             <div id="logo">
-                <img id="navLogo" alt="Social Content +"src={`./img/logo1.jpg`}></img>
+                <img id="navLogo" alt="Social Content +"src={`./img/logo_light.png`}></img>
             </div>
             <ul id="navButtons">
                 <li id="homeButton" className="navButton">
                     Home
                 </li>
-                <li id="aboutButton" className="navButton">
-                    Contact
-                </li>
                 <li id="contactButton" className="navButton">
                     About
+                </li>
+                <li id="aboutButton" className="navButton">
+                    Contact
                 </li>
             </ul>
         </div>
