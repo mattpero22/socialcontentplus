769224d7ac85671b77635305ccd0a5a38a1dc311
matt pero <mattpero22@gmail.com>
2023-02-28 10:03:12 -0500

    began construction of email form for site using EmailJS

--- main/src/pages/Contact.jsx
+++ main/src/pages/Contact.jsx
@@ -1,7 +1,21 @@
+import emailjs from 'emailjs-com'
+
+
 const Contact = () => {
+    
+    const sendEmail = (e) => {
+        e.preventDefault()
+        console.log(e)
+    }
+
     return <>
     <div id="contact" className="page">
         Contact Page
+        <form id="contactForm" onSubmit={sendEmail}>
+            <label>Name</label>
+            <input type="text" name="from_name" />
+            <input type="submit" value="Send" />
+        </form>
     </div>
     </>
 }
